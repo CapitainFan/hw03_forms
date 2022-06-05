5f138ea31e456b2db24cd4e5ad3528a66e057739
CapitainFan <gudokrulit@mail.ru>
2022-06-05 12:10:48 +0300

    pytest

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -1,11 +1,10 @@
-from django.forms import ModelForm
-
+from django import forms
 from .models import Post
 
 
-class PostForm(ModelForm):
-    class Meta:
+class PostForm(forms.ModelForm):
+    class Meta():
         model = Post
-        labels = {'group': 'Группа', 'text': 'Сообщение'}
+        labels = {'group': 'Группа', 'text': 'Напишите свой пост'}
         help_texts = {'group': 'Выберите группу', 'text': 'Введите ссообщение'}
-        fields = ["group", "text"]
+        fields = ('text', 'group')
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,7 +5,6 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
@@ -15,13 +14,19 @@ class Group(models.Model):
 
 
 class Post(models.Model):
-    name = models.CharField(max_length=200, default='unknown')
     text = models.TextField()
-    pub_date = models.DateTimeField('date published', auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name='posts')
-    group = models.ForeignKey(Group, on_delete=models.SET_NULL,
-                              related_name='posts', blank=True, null=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts')
 
     class Meta:
         ordering = ['-pub_date']
