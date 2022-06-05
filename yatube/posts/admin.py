6faa9695fdc986bca28f338fdc7475add1a85676
CapitainFan <gudokrulit@mail.ru>
2022-06-05 13:43:54 +0300

    pytest

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,19 +2,28 @@ from django.contrib import admin
 from .models import Post, Group
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("pk", "text", "pub_date", "author")
-    search_fields = ("text",)
-    list_filter = ("pub_date",)
-    empty_value_display = "-пусто-"
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group',
+    )
+    list_editable = ('group',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
 
 
-class PostGroup(admin.ModelAdmin):
-    list_display = ("title", "description")
-    search_fields = ("description",)
-    list_filter = ("title",)
-    empty_value_display = "-пусто-"
-
-
-admin.site.register(Post, PostAdmin)
-admin.site.register(Group, PostGroup)
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'slug',
+        'description',
+    )
+    search_fields = ('title',)
+    empty_value_display = '-пусто-'
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,8 @@
 from django.contrib.auth.decorators import login_required
-
 from django.core.paginator import Paginator
-
 from django.shortcuts import render, get_object_or_404, redirect
-
 from django.conf import settings
-
 from .models import Post, Group, User
-
 from .forms import PostForm
 
 
