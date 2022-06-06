eb92ff2daab4cc0d2a03305369218e8b917db668
CapitainFan <gudokrulit@mail.ru>
2022-06-06 09:03:45 +0300

    a bit more tiny corrects

--- yatube/core/context_processors/year.py
+++ yatube/core/context_processors/year.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 
 def year(request):
-    """Добавляет переменную с текущим годом."""
     current_year = datetime.now().year
     return {
         "year": current_year
