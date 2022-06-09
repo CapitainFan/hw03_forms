10e57aa43db9770116fa61b13b3dc85a30fdb5ba
CapitainFan <gudokrulit@mail.ru>
2022-06-09 13:41:07 +0300

    5th sprint

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -7,11 +7,11 @@ SECRET_KEY = 'bw!2zrgtrrdzzrccwq$qo=2venmmcjt5so3jip2=6h+9uvw0g+'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
     'localhost',
+    '127.0.0.1',
+    '[::1]',
     'testserver',
 ]
-
 
 INSTALLED_APPS = [
     'posts',
